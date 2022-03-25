55a770a12e0b5be2e6231d1f670fec704e0799b7
Natalia Maximo <iam@natalia.dev>
2022-03-25 14:37:08 -0400

    cleared branch for golang migration

--- scripts/nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-nginx;
-inotifywait -m -r -e modify -e create -e move -e delete /etc/nginx | 
-while read events; do
-    nginx -s reload;
-done;
--- scripts/parser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p ./nginx/sites-enabled ./nginx/routes
-python3 ./parser/parser.py;
-cp ./nginx/sites-available/* ./nginx/sites-enabled;
-inotifywait -r -e modify -m /app/data |
-while read events; do
-    rm -r ./nginx/routes/*;
-    python3 ./parser/parser.py;
-done;
