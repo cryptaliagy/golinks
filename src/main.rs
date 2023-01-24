2e74e7029b3e20cfcf720de784863bc69404726a
Natalia Maximo <nmaximo@microsoft.com>
2023-01-24 16:26:18 -0500

    refactor into modules

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,17 @@
+mod models;
+mod utils;
+
 #[macro_use]
 extern crate rocket;
 
-use std::collections::HashMap;
 use std::env;
 use std::fs;
-use std::time::{Duration, SystemTime};
 
-use chrono::Utc;
-
-use rocket::fairing::{Fairing, Info, Kind};
 use rocket::response::Redirect;
-use rocket::serde::{json::Json, Deserialize, Serialize};
-use rocket::{Data, Request, Response, State};
+use rocket::serde::json::Json;
+use rocket::{Build, Request, Rocket, State};
 
-/// Fairing for timing requests.
-struct RequestTimer;
-
-/// Value stored in request-local state.
-#[derive(Copy, Clone)]
-struct TimerStart(Option<SystemTime>);
-
-#[rocket::async_trait]
-impl Fairing for RequestTimer {
-    fn info(&self) -> Info {
-        Info {
-            name: "Request Timer",
-            kind: Kind::Request | Kind::Response,
-        }
-    }
-
-    /// Stores the start time of the request in request-local state.
-    async fn on_request(&self, request: &mut Request<'_>, _: &mut Data<'_>) {
-        // Store a `TimerStart` instead of directly storing a `SystemTime`
-        // to ensure that this usage doesn't conflict with anything else
-        // that might store a `SystemTime` in request-local cache.
-        request.local_cache(|| TimerStart(Some(SystemTime::now())));
-    }
-
-    /// Adds a header to the response indicating how long the server took to
-    /// process the request.
-    async fn on_response<'r>(&self, req: &'r Request<'_>, res: &mut Response<'r>) {
-        let start_time = req.local_cache(|| TimerStart(None));
-        if let Some(Ok(duration)) = start_time.0.map(|st| st.elapsed()) {
-            println!(
-                "{time} | {method:^7} | {duration:>12} | {status} | \"{uri}\"",
-                time = Utc::now().format("%Y-%m-%d - %H:%M:%S"),
-                method = req.method(),
-                uri = req.uri(),
-                duration = format_duration(duration),
-                status = res.status().code,
-            );
-        }
-    }
-}
-
-/// Struct for serializing a status message.
-#[derive(Serialize)]
-#[serde(crate = "rocket::serde")]
-struct StatusMessage {
-    status: String,
-    message: String,
-}
-
-/// Struct for deserializing routes
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(crate = "rocket::serde")]
-struct Routes {
-    routes: HashMap<String, String>,
-}
-
-/// Formats a `Duration` as a string.
-fn format_duration(duration: Duration) -> String {
-    let millis = duration.as_millis();
-    let micros = duration.subsec_micros() as u128 - millis * 1000;
-    let nanos = duration.subsec_nanos() as u128 - micros * 1000;
-    if millis > 0 {
-        format!("{}.{} ms", millis, micros)
-    } else {
-        format!("{}.{} μs", micros, nanos)
-    }
-}
+use crate::models::{RequestTimer, Routes, StatusMessage};
 
 #[catch(404)]
 fn not_found(req: &Request) -> Json<StatusMessage> {
@@ -101,9 +33,20 @@ fn heartbeat() -> Json<StatusMessage> {
 fn link(link: &str, routes: &State<Routes>) -> Option<Redirect> {
     routes
         .inner()
-        .routes
-        .get(link)
-        .map(|url| Redirect::temporary(url.clone()))
+        .fetch(link)
+        .map(|url| Redirect::temporary(url))
+}
+
+fn build_rocket(routes: Routes, enable_profiling: bool) -> Rocket<Build> {
+    let ship = if enable_profiling {
+        rocket::build().attach(RequestTimer {})
+    } else {
+        rocket::build()
+    };
+
+    ship.manage(routes)
+        .mount("/", routes![heartbeat, link])
+        .register("/", catchers![not_found])
 }
 
 #[launch]
@@ -117,13 +60,5 @@ fn rocket() -> _ {
     let routes: Routes = serde_yaml::from_reader(config_file)
         .expect(format!("Unable to parse {}", &links_file).as_str());
 
-    let ship = if enable_profiling {
-        rocket::build().attach(RequestTimer {})
-    } else {
-        rocket::build()
-    };
-
-    ship.manage(routes)
-        .mount("/", routes![heartbeat, link])
-        .register("/", catchers![not_found])
+    build_rocket(routes, enable_profiling)
 }
